b16a0c5164a149accbed819836f99a9b6613f1a8
Eugene <lehongthai2000@gmail.com>
2022-12-02 10:11:18 +0700

    navbar first implementation

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
     </div>
   );
 }
 
 export default App;
+
+
+// https://blog.logrocket.com/how-create-multilevel-dropdown-menu-react/
+// https://react-bootstrap.github.io/components/navbar/
