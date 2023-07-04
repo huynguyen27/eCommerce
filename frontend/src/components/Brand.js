a7f8be6675eae09b205272a8c68b7cbd69d7df3e
henry <vin734@usask.ca>
2023-07-04 16:53:33 +0700

    fe-initialize routes and homepage

---
+++ frontend/src/components/Brand.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Brand = () => {
+  return (
+    <div>
+      <h2>Brand</h2>
+      <p>This is the Brand page.</p>
+    </div>
+  );
+};
+
+export default Brand;
---
+++ frontend/src/components/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h2>ShoppingCart</h2>
+      <p>This is the LoginRegister page.</p>
+    </div>
+  );
+};
+
+export default ShoppingCart;
