9f2b1806ba491eee1bdbed36c6bda928febe9873
HuyTu <dragon10x0408@gmail.com>
2023-07-09 22:14:46 +0700

    init db

---
+++ backend/db/dbConnect.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.DBURL;
+
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+module.exports = mongoose
